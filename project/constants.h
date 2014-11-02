83c16ebd1ae45e440184a9b120c59eccbdda2dd1
Maciej <m.szeszko@student.uw.edu.pl>
2014-11-02 17:09:44 +0100

    Constants schema modification.

--- project/constants.h
+++ project/constants.h
@@ -6,41 +6,42 @@
 
 enum WorkerMode {
 	ready = 0 // Created and initialized, but not active yet.
-	compute, // 
-	send, // Send processed data to the predecessor.
-	closed // Wait for child process, free allocated resources and exit.
+	compute = 1, // 
+	send = 2, // Send processed data to the predecessor.
+	closed = 3 // Wait for child process, free allocated resources and exit.
 };
 
 enum PascalMode {
-	initChain = 10, // Chain of worker processes still does not exist.
-	commitComputation, //
-	gatherResults, //
-	waitAndClose // --------||--------
+	initChain = 0, // Chain of worker processes still does not exist.
+	commitComputation = 1, //
+	gatherResults = 2, //
+	waitAndClose = 3 // --------||--------
 };
 
 enum Token {
-	init = 20, //
-	compute, // 
-	gatherResults, //
-	waitAndClose
+	init = 0, //
+	compute = 1, // 
+	gatherResults = 2, //
+	waitAndClose = 3
 };
 
-struct PascalMsg {
-	int token = 40
-};
+
+// Success message:
+const char success = '#';
 
 struct ProcessConfirmMsg {
 	// After accomplishing request for speficic token,
 	// process is obliged to return confirmation message to the predecessor.
-	int msg = 50
+	char msg;
 };
 
+// 'Y' stands for "Yes, I'm the last one.".
 struct TriangleCeofficient {
-	int ceofficient,
-	int isLast
+	int ceofficient;
+	char isLast;
 };
 
-struct ProcessMsg {
-	int token; // One of tokens from Token enum.
-	int workersLeft, // Number of workers required for complete current row.
+struct RequestMsg {
+	enum Token token; // One of tokens from Token enum.
+	int workersLeft; // Number of workers required for complete current row.
 };
